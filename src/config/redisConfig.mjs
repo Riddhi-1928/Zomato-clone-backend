5a30683dc297f58cbc0e2b1197595b3601be6c45
Riddhi Bhoyar <riddzzz.28@gmail.com>
2025-05-30 01:06:15 +0530

    redis password added

--- src/config/redisConfig.mjs
+++ src/config/redisConfig.mjs
@@ -3,11 +3,16 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const redisClient = createClient({
   socket: {
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
+     ...(isProduction && { tls: true }), // Use TLS only in production
   },
+   ...(isProduction && { password: process.env.REDIS_PASSWORD }), // Use password only in production
+
 });
 redisClient.on("connect", () => console.log(" Redis Connected"));
 redisClient.on("error", (err) => console.error(" Redis Error:", err));
